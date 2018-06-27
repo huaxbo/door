6acc29c97436e134dc55c59592ffa8ccc446953d
865507207 <865507207@qq.com>
2018-06-27 10:12:07 +0800

    极光推送

---
+++ src/main/java/com/automic/door/util/pusher/JgPusher.java
@@ -0,0 +1,60 @@
+package com.automic.door.util.pusher;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.automic.global.util.ConfigGloUtil;
+
+import cn.jiguang.common.ClientConfig;
+import cn.jiguang.common.resp.APIConnectionException;
+import cn.jiguang.common.resp.APIRequestException;
+import cn.jpush.api.JPushClient;
+import cn.jpush.api.push.PushResult;
+import cn.jpush.api.push.model.PushPayload;
+
+public class JgPusher {
+
+	private static Logger log = LogManager.getLogger(JgPusher.class);
+
+	/**
+	 * @param mess
+	 */
+	public static void pushMess(String mess,String title) {
+		JPushClient jpushClient = new JPushClient(
+				ConfigGloUtil.getElementText("jg_secret"),
+				ConfigGloUtil.getElementText("jg_key"), null,
+				ClientConfig.getInstance());
+
+		// For push, all you need do is to build PushPayload object.
+		PushPayload payload = buildPushObject_all_all_alert(mess);
+		try {
+			PushResult result = jpushClient.sendPush(payload);
+			
+			log.info(result.getOriginalContent());
+			
+		} catch (APIConnectionException e) {
+			// Connection error, should retry later
+			log.error("Connection error, should retry later", e);
+
+		} catch (APIRequestException e) {
+			// Should review the error, and fix the request
+			log.error("Should review the error, and fix the request", e);
+			log.info("HTTP Status: " + e.getStatus());
+			log.info("Error Code: " + e.getErrorCode());
+			log.info("Error Message: " + e.getErrorMessage());
+		}finally{}
+	}
+
+	 /**
+	 * @param mess
+	 * @return
+	 */
+	private static PushPayload buildPushObject_all_all_alert(String mess) {
+	        return PushPayload.alertAll(mess);
+	 }
+		
+	
+	public static void main(String[] args){
+		pushMess("信息推送测试","门控信息");
+	}
+}
